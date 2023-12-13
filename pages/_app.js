5ab047f5b0ffadd0f50ea8c8ae2e812c1672a219
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2023-12-14 00:42:13 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,28 +6,31 @@ import { configureChains, createConfig, WagmiConfig } from "wagmi";
 
 import { publicProvider } from "wagmi/providers/public";
 import { xdcdevnet, xdcsubnet } from "@/config";
+import { useState } from "react";
 
-const { chains, publicClient } = configureChains(
-  [xdcdevnet, xdcsubnet],
-  [publicProvider()]
-);
-const { connectors } = getDefaultWallets({
-  appName: "App",
-  projectId: "2a612b9a18e81ce3fda2f82787eb6a4a",
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
 export default function App({ Component, pageProps }) {
+  const [data, setData] = useState({ chainsUrls: [xdcdevnet, xdcsubnet] });
+
+  const { chains, publicClient } = configureChains(data.chainsUrls, [
+    publicProvider(),
+  ]);
+  const { connectors } = getDefaultWallets({
+    appName: "App",
+    projectId: "2a612b9a18e81ce3fda2f82787eb6a4a",
+    chains,
+  });
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  });
   const rainbowKitConfig = {
     chains: chains,
     showRecentTransactions: true,
     coolMode: true,
   };
+
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider {...rainbowKitConfig}>
