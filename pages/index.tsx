7d3e41d58eba0117c1e9e6482ac0b47518fd89d0
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-01-23 18:05:43 +0800

    Go to bridge route directly (#5)
    Go to bridge route directly
    isCurrentNetwork

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/bridge");
+  }, []);
+
   return <div className="text-center mt-64 text-5xl text-white">Subswap</div>;
 }
